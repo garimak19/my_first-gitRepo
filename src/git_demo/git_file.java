fc763eae24457ebd227511d52ccd31865d3f4320
garimak19 <lovelygarima@gmail.com>
2020-04-13 14:43:31 -0700

    added info master

--- src/git_demo/git_file.java
+++ src/git_demo/git_file.java
@@ -4,7 +4,7 @@ public class git_file {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" inside branch 1");
+		System.out.println(" inside master");
 		
 
 	}
