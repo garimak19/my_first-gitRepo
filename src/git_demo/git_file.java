dffbfd3de2194771d54795592a2af70339b5184f
garimak19 <lovelygarima@gmail.com>
2020-04-13 14:50:51 -0700

    merge commit

--- src/git_demo/git_file.java
+++ src/git_demo/git_file.java
@@ -4,13 +4,12 @@ public class git_file {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		
 		System.out.println("inside branch 1");
-=======
+
 		System.out.println(" inside master");
-		
->>>>>>> master
+
 
 	}
 
